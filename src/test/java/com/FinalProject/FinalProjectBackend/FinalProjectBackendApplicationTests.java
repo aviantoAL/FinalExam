30bbe4795fe8d10ed57d8bd28de04e3360488263
aviantoAL <102365326+aviantoAL@users.noreply.github.com>
2024-04-02 20:11:25 +0700

    first commit

---
+++ src/test/java/com/FinalProject/FinalProjectBackend/FinalProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FinalProject.FinalProjectBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
